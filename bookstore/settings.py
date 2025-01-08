f4fbd44112edf0e8ae8a3c9868bb1e24c71d68d4
MylenaFDS <mylenafds@gmail.com>
2025-01-08 20:20:02 -0300

    mudança no views.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -57,19 +57,20 @@ ROOT_URLCONF = "bookstore.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'bookstore' / 'templates'],  # Corrija o caminho para o diretório templates
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = "bookstore.wsgi.application"
 
