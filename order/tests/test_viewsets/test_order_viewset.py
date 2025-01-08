1b6a6131b9e3f122e41fa2a9563df33260ff096a
MylenaFDS <mylenafds@gmail.com>
2025-01-08 20:41:26 -0300

    Corrigindo problemas nos testes de order

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -23,24 +23,31 @@ class TestOrderViewSet(APITestCase):
 
     def test_order(self):
         response = self.client.get(
-            reverse("order-list", kwargs={"version": "v1"}))
-
+        reverse("order-list", kwargs={"version": "v1"}))
+    
+    # Verifica se o status de resposta é 200 OK
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
+    
+    # Imprime o conteúdo retornado para inspecionar a estrutura
         order_data = json.loads(response.content)
+        print(json.dumps(order_data, indent=2))  # Adicione esta linha para inspecionar a resposta
+
+    # Testa os campos do JSON retornado
         self.assertEqual(
-            order_data["results"][0]["product"][0]["title"], self.product.title
+            order_data[0]["product"][0]["title"], self.product.title
         )
         self.assertEqual(
-            order_data["results"][0]["product"][0]["price"], self.product.price
+            order_data[0]["product"][0]["price"], self.product.price
         )
         self.assertEqual(
-            order_data["results"][0]["product"][0]["active"], self.product.active
+            order_data[0]["product"][0]["active"], self.product.active
         )
         self.assertEqual(
-            order_data["results"][0]["product"][0]["category"][0]["title"],
+            order_data[0]["product"][0]["category"][0]["title"],
             self.category.title,
         )
+
+    
 
     def test_create_order(self):
         user = UserFactory()
