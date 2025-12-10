39f2eece31e292c00b1fec358dac61eb5ef57da5
MylenaFDS <mylenafds@gmail.com>
2025-12-10 00:39:43 -0300

    ....

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -51,9 +51,6 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer"
-    ] if not DEBUG else [
-        "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer",
     ]
 }
 
